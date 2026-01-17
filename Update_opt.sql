701661077b47265d749eac8eb38ab57d4158b79d
Visha1005 <mainvishu.p3213@gmail.com>
2026-01-17 10:28:31 +0530

    Add files via upload

---
+++ Update_opt.sql
@@ -0,0 +1,17 @@
+USE harry_sql;
+-- UPDATE user SET name = "VISHU" WHERE id = 1;
+
+-- MULTIPLE COLUMNS IN ONE QUERY
+-- UPDATE user SET name = "OMYA",email = "om@niphad.com" WHERE ID = 5;
+
+-- SETTING SALARY FOR ALL THE EMPLOYEES
+-- UPDATE user SET salary= 80000 WHERE ID =1;
+-- UPDATE user SET salary= 76000 WHERE ID =2;
+-- UPDATE user SET salary= 31000 WHERE ID =3;
+-- UPDATE user SET salary= 92000 WHERE ID =4;
+-- UPDATE user SET salary= 47000 WHERE ID =5;
+-- UPDATE user SET salary= 78000 WHERE ID =6;
+-- UPDATE user SET salary= 80500 WHERE ID =7;
+-- UPDATE user SET salary= 39000 WHERE ID =8;
+-- UPDATE user SET salary= 85000 WHERE ID =9;
+SELECT * FROM user;
