19812aeb68342459e182b1e034a4e8e06a4c2b95
Vishu200605 <mainvishu.p3213@gmail.com>
2026-02-01 11:14:55 +0530

    Add files via upload

---
+++ Views.sql
@@ -0,0 +1,17 @@
+USE harry_sql;
+-- ALTER TABLE user DROP dob ;
+-- INSERT INTO user(ID,name,gender,salary)  VALUES 
+-- (21,"Kartik","Male",21000),
+-- (22,"Radhika","Female",35000),
+-- (23,"Krishna","Male",29000),
+-- (24,"Mukesh","Male",56000);
+
+-- DESCRIBE user;
+-- CREATE VIEW HIGH_PAYERS AS
+-- SELECT ID,name FROM user WHERE salary >50000;
+-- CREATE VIEW  lower_salary AS
+-- SELECT name,salary FROM 
+-- user WHERE salary<30000;
+-- SELECT *  FROM HIGH_PAYERS;
+-- SELECT *  FROM HIGH_PAYERS;
+-- SELECT * FROM lower_salary;
